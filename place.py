c94b8497f35cc863564ea4bb1188fc5d1522b7f9
Tomas <tomasbuci@gmail.com>
2026-01-22 17:37:03 +0000

    dunno

--- place.py
+++
@@ -1,5 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('../results.db')
-conn.execute("INSERT INTO vocabulary (english, slovak, image) VALUES ('place','miesto','place.png')")
-conn.commit()
-conn.close()
